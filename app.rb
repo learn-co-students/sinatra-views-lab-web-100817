19b1af05b82b91bc57269e7e0910ee4638eca3d9
evaccaro <erv230@nyu.edu>
2017-10-30 15:36:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
+require 'date'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@day = Time.now.strftime("%A").to_s
+		@month = Time.now.strftime("%B").to_s
+		@num = Time.now.strftime("%e").to_s
+		@year = Time.now.strftime("%Y").to_s 
+		erb :date
 	end
 
 
